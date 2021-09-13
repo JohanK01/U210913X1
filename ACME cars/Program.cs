072d9724763646a7762cce43af67650cb6959d75
JohanK <klausenjohan23@gmail.com>
2021-09-13 11:29:02 +0200

    menyn är färdig

--- ACME cars/Program.cs
+++ ACME cars/Program.cs
@@ -6,7 +6,40 @@ namespace ACME_cars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.WriteLine("X = Exit, N=Add Car, E=Edit existing Car, D=Remove Car, S=Show all cars");
+                string orden = Console.ReadLine();
+
+                if (orden == "x")
+                {
+                    break;
+                }
+                if (orden == "n || N")
+                {
+                    Console.WriteLine("Vilken modell");
+                }
+                if (orden == "e || E")
+                {
+                    Console.WriteLine("Vilken bil vill du ändra");
+                }
+                if (orden == "d || D")
+                {
+                    Console.WriteLine("Vilken bil vill du ta bort");
+                }
+                if (orden == " s ||S")
+                {
+                    Console.WriteLine("Visa alla bilar");
+                }
+            }
+
         }
+    }
+
+    public class Bsalu
+    {
+
+
+
     }
 }
