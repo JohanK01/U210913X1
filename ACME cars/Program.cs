07de63a23e1a33d43da82521462fb030c215304a
JohanK <klausenjohan23@gmail.com>
2021-09-13 11:15:57 +0200

    Start

---
+++ ACME cars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ACME_cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
